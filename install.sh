68ef4561593edb2ab82805ef5da307c50d4630b7
Jack <14117126+jdipper@users.noreply.github.com>
2022-04-14 12:45:29 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ARCH=$(dpkg --print-architecture)
+CSVERSION="4.2.0"
+CSDOWNLOAD="https://github.com/coder/code-server/releases/download/v{$CSVERSION}/code-server_{$CSVERSION}_{$ARCH}.deb"
+curl -fOL $CSDOWNLOAD
+sudo dpkg -i code-server_{$VERSION}_{$ARCH}.deb
