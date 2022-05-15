b26a297bbd09d20c8e90f515680e62a566bbdd4d
Jack <14117126+jdipper@users.noreply.github.com>
2022-05-15 19:15:21 +0100

    Update and rename install.sh to install/code-server.sh

--- install/code-server.sh
+++ install/code-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ARCH=$(dpkg --print-architecture)
-CSVERSION="4.2.0"
+CSVERSION="4.4.0"
 CSDOWNLOAD="https://github.com/coder/code-server/releases/download/v${CSVERSION}/code-server_${CSVERSION}_${ARCH}.deb"
 curl -fOL $CSDOWNLOAD
 sudo dpkg -i code-server_${CSVERSION}_${ARCH}.deb
