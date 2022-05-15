ff72e4b89ae04f783375aa67e92009205ed5b32c
Jack <14117126+jdipper@users.noreply.github.com>
2022-05-15 19:20:01 +0100

    Update code-server.sh

--- install/code-server.sh
+++ install/code-server.sh
@@ -3,4 +3,6 @@ ARCH=$(dpkg --print-architecture)
 CSVERSION="4.4.0"
 CSDOWNLOAD="https://github.com/coder/code-server/releases/download/v${CSVERSION}/code-server_${CSVERSION}_${ARCH}.deb"
 curl -fOL $CSDOWNLOAD
-sudo dpkg -i code-server_${CSVERSION}_${ARCH}.deb
+dpkg -i code-server_${CSVERSION}_${ARCH}.deb
+systemctl enable code-server@ubuntu
+systemctl start code-server@ubuntu
