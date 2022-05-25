9527037c80057c31c762321f4a7e06f2cbdb437e
Jack <14117126+jdipper@users.noreply.github.com>
2022-05-25 08:30:31 +0100

    Re-inserted line

--- install/postgresql.sh
+++ install/postgresql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#sudo useradd -p "$6$cnJkE.7z$.TaPEk2z1JLeS6qNh/dGO5/wD5tNLSw2XklozQrv4xfbBccwqC4vBU.un.HCcTXo500X0.Kffw9oTJ2knVNS2/" devdb
 sudo -u postgres createuser devdb
 sudo -u postgres createdb devdb  
+sudo useradd -p "$6$cnJkE.7z$.TaPEk2z1JLeS6qNh/dGO5/wD5tNLSw2XklozQrv4xfbBccwqC4vBU.un.HCcTXo500X0.Kffw9oTJ2knVNS2/" devdb
 #sudo -u postgres psql -c "alter user devuser with encrypted password 'password'; grant all privileges on database devdb to devuser;" 
