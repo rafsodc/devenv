955f1afee6d4ed6f6d10c7e5670add7667c28b10
Jack <14117126+jdipper@users.noreply.github.com>
2022-10-09 18:47:29 +0100

    Update start.sh

--- local/start.sh
+++ local/start.sh
@@ -5,6 +5,3 @@
 sudo docker-compose --env-file .env.local build
 sudo docker-compose --env-file .env.local up -d
 
-# Start message consumer
-sudo docker-compose -f /var/www/sodc-api/docker-compose.yml exec php php bin/console messenger:consume email_priority_high email_priority_normal email_priority_low -vv
-
