318308c0fcb924863aca2d3de048fa9ad7bce6b8
Jack <14117126+jdipper@users.noreply.github.com>
2023-07-14 21:58:47 +0100

    Update code-server.sh

--- install/code-server.sh
+++ install/code-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ARCH=$(dpkg --print-architecture)
-CSVERSION="4.11.0"
+CSVERSION="4.14.1"
 CSDOWNLOAD="https://github.com/coder/code-server/releases/download/v${CSVERSION}/code-server_${CSVERSION}_${ARCH}.deb"
 curl -fOL $CSDOWNLOAD
 dpkg -i code-server_${CSVERSION}_${ARCH}.deb
